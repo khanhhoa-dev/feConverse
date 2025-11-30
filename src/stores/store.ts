7440324aedd162cc1c19dfa739569d53e58db1c5
LêKhánhHòa <mcskhanhhoa@gmail.com>
2025-11-30 11:02:07 +0700

    fix: delete library persist

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,37 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../stores/Slices/authSlice';
-import storage from 'redux-persist/lib/storage';
-import {
-    persistReducer,
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import {
+//     persistReducer,
+//     persistStore,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+// } from 'redux-persist';
 
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['login'],
-};
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['login'],
+// };
 
-const persistAuthReducer = persistReducer(persistConfig, authReducer);
+// const persistAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
-        auth: persistAuthReducer,
+        auth: authReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+    // middleware: (getDefaultMiddleware) =>
+    //     getDefaultMiddleware({
+    //         serializableCheck: {
+    //             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    //         },
+    //     }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>; // Type cho toàn bộ state
 export type AppDispatch = typeof store.dispatch; // Type cho dispatch (hỗ trợ thunks)
