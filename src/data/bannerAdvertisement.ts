695a031c926dac9501abf337da12b50631908a89
LêKhánhHòa <mcskhanhhoa@gmail.com>
2025-12-12 16:27:07 +0700

    fix: Update Banner

--- src/data/bannerAdvertisement.ts
+++ src/data/bannerAdvertisement.ts
@@ -5,15 +5,15 @@ interface IBanner {
 
 export const Banners: Record<string, IBanner> = {
     Sale: {
-        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/2/8/2800x1300_main_4.gif',
+        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/c/o/converse-x-madhappy-eng-main-desktop-.gif',
         link: 'shoes',
     },
     CocaShoes: {
-        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/c/o/coke_exclusive_launch_desktop_shop_2x.jpg',
+        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/s/t/stranger_things_main_1_.jpg',
         link: 'shoes',
     },
     Shoes: {
-        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/s/h/shine_eng_main_desktop_.jpg',
+        image: 'https://www.converse.vn/media/weltpixel/owlcarouselslider/images/d/e/dekstop_design_main_banner_49.jpg',
         link: 'shoes',
     },
 } as const;
