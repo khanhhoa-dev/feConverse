c6dafa48594efd04d2796d0c398bb092ff2beb4c
LêKhánhHòa <mcskhanhhoa@gmail.com>
2025-12-13 16:04:48 +0700

    cea: create hook useAccessToken

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -5,3 +5,8 @@ export const useLoginSelector = () => {
     const userData = useSelector((state: RootState) => state.auth.login.data);
     return userData;
 };
+
+export const useAccessToken = () => {
+    const accessToken = useSelector((state: RootState) => state.auth.accessToken);
+    return accessToken;
+};
